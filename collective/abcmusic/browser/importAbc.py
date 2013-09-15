d26a4649fe32bc072491ff2dc54ce20bee5f11ff
Eric Hardy <Eric.Hardy@univ-brest.fr>
2013-09-15 10:45:11 +0200

    import abc in progess

--- collective/abcmusic/browser/importAbc.py
+++ collective/abcmusic/browser/importAbc.py
@@ -1,4 +1,21 @@
+# usefull documentations :
+# http://developer.plone.org/forms/z3c.form.html
+# http://developer.plone.org/forms/files.html
+# http://pythonhosted.org/z3c.form/
+
 import logging
+from five import grok
+from plone.directives import form
+
+from zope import schema
+from z3c.form import button
+
+from Products.CMFCore.interfaces import ISiteRoot
+from Products.statusmessages.interfaces import IStatusMessage
+
+from plone.namedfile.field import NamedFile
+from plone.i18n.normalizer import idnormalizer
+
 from zope.publisher.browser import BrowserView
 from Products.CMFCore.utils import getToolByName
 from AccessControl import getSecurityManager
@@ -8,6 +25,36 @@ from collective.abcmusic import _
 
 logger = logging.getLogger('collective.abcmusic')
 
-class importAbc(BrowserView):
-    def __call__(self):
-        return 'dans importAbc'
+class IImportABC(form.Schema):
+    abc_file = NamedFile(
+            title=_(u"import a file containing one or more abc tunes"),
+        )
+
+class importAbc(form.SchemaForm):
+    grok.name('importABC')
+    grok.require('zope2.View')
+    grok.context(ISiteRoot)
+    
+    schema = IImportABC
+    ignoreContext = True
+    
+    label = _(u"import abc file")
+    description = _(u"This will create abctune(s)")
+    
+    @button.buttonAndHandler(u'Ok')
+    def handleApply(self, action):
+        data, errors = self.extractData()
+        if errors:
+            self.status = self.formErrorsMessage
+            return
+
+        # Do something with valid data here
+
+        # Set status on this form page
+        # (this status message is not bind to the session and does not go thru redirects)
+        self.status = "Thank you very much!"
+
+    @button.buttonAndHandler(u"Cancel")
+    def handleCancel(self, action):
+        """User cancelled. Redirect back to the front page.
+        """
